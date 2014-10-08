b0d9103a584a8291a4baf528df271adf0c93dfc4
nils_thunder <nils.brueggemann@zmaw.de>
2014-10-08 18:34:46 +0200

    Standard backup

--- myqg.f90
+++ myqg.f90
@@ -5,18 +5,31 @@ program main
 
   integer :: i,j,k,l,n
   character(len=128) :: fname
-  character(len=48) :: fprfx
+  character(len=128) :: fprfx
+  character(len=128) :: path_data
+  integer, dimension(3) :: dims
   integer :: nt
 
-  nx = 120
-  ny = 60
+  nx = 100
+  nx = 10
+  ny = 80
+  ny = 5
   nz = 2
+  path_data = "/scratch/uni/ifmto/u241161/myqg/test/"
 
   call allocate_myqg_module
 
-  dx = 10.e3
-  dy = 10.e3
-  dz = 5.
+  dx = 10e3
+  dy = 10e3
+  dz = (/ 50., 50. /)
+
+  nt = 1000
+  dt = 1.e2
+  tstep = 0
+  t_start = dt * tstep
+  time    = t_start
+  t_end   = dt * nt
+  timeio  = t_end / 10. 
 
   do i=1,nx
     xu(i) = dx*(i-1)
@@ -28,43 +41,97 @@ program main
     yt(j)=dy*(j-0.5)
   enddo 
 
-  Lx = nx*dx
-  Ly = ny*dy
+  do k=2,nz
+    zu(k)=zu(k-1) + dz(k)
+  enddo
+  do k=1,nz
+    zt(k)=zu(k) + 0.5*dz(k)
+  enddo
 
   do i=1,nx
     do j=1,ny
-      u(i,j,1) = sin(4*xu(i)/Lx*2*pi)*cos(yu(j)/Ly*2*pi)
-      !u(i,j,1) = (xu(i)/Lx)**2 + (yu(j)/Ly)**2 
-      !u(i,j,1) = i*j
-      if (i == 0) then
-        write(*,*) u(i,j,1)
-      endif
+      do k=1,nz
+        recepvol(i,j,k) = 1.0 / (dx*dy*dz(k))
+      enddo
     enddo
   enddo
-  fprfx = "test"
-  call write_3d(fprfx, u)
- 
-  nt = 1000
-  dt = 10.
-  tstep = 0
-  t_start = dt * tstep
-  time    = t_start
-  t_end   = dt * nt
+
+  Lx = nx*dx
+  Ly = ny*dy
+
+  do i=1-ox,nx+ox
+    do j=1-ox,ny+ox
+      do k=1,nz
+        !u(i,j,k) = sin(4*xu(i)/Lx*2*pi)*cos(yu(j)/Ly*2*pi)
+        !u(i,j,k) = (xu(i)/Lx)**2 + (yu(j)/Ly)**2 
+        !u(i,j,k) = i*j*k
+        u(i,j,k) = 1.
+      enddo
+    enddo
+  enddo
+  !fprfx = "test"
+  !call write_3d(fprfx, u)
+
+  do i=1-ox,nx+ox
+    do j=1-ox,ny+ox
+      do k=1,nz
+        pv(i,j,k) = exp(-( ((xt(i)-Lx/2.)/Lx)**2 + ((yt(j)-Ly/2.)/Ly)**2 ))
+        pv(i,j,k) = j*nx + i
+      enddo
+    enddo
+  enddo
+
+  write(*,*) sngl(pv(:,1,1))
+  call cyclic_exchange(pv)
+  write(*,*) "start"
+  do j=1-ox,ny+ox
+    write(*,*) sngl(pv(:,j,1))
+  enddo
+  stop
+
+  write(*,*) "Finished: Initialize variables"
+
+
+  ! write grid data out
+  call write_3d("XC        ", xt, (/ nx,  1,  1 /), -1, path_data)
+  call write_3d("YC        ", yt, (/  1, ny,  1 /), -1, path_data)
+  call write_3d("RC        ", zt, (/  1,  1, nz /), -1, path_data)
+  call write_3d("XG        ", xu, (/ nx,  1,  1 /), -1, path_data)
+  call write_3d("YG        ", yu, (/  1, ny,  1 /), -1, path_data)
+  !call write_3d("RF        ", zu, (/  1,  1, nz /), -1, path_data)
+  call write_3d("RF        ", (/ zu(:), 0.d0 /), (/  1,  1, nz+1 /), -1, path_data)
 
   ! main time stepping loop
+  write(*,*) "Start: Time stepping"
   do while ( time < t_end )
-    write(*,*) tstep
     tstep = tstep + 1
     time  = tstep*dt
 
+    ! calculate tendencies
     call calc_Gpv
-    pv = pv + ( (1.5+epsab)*Gpv - (0.5+epsab)*Gpvm1 ) *dt
+
+    ! pv forward step
+    if ( tstep==1 ) then
+      Gpvm1 = Gpv
+    endif
+    pv(1:nx,1:ny,1:nz) = pv(1:nx,1:ny,1:nz) + ( (1.5+epsab)*Gpv - (0.5+epsab)*Gpvm1 ) * dt
+
+    ! reset tendencies
     Gpvm1 = Gpv
+    Gpv = 0.0
+
+    ! do cyclic_exchange (Does this occur at correct place???)
+    !call cyclic_exchange(u)
+    !call cyclic_exchange(v)
+    !call cyclic_exchange(psi)
+    call cyclic_exchange(pv)
 
     ! do model I/O
-    if ( tstep/tstepio == tstep/tstepio ) then
-      fprfx = "pv." // tstepform
-      call write_3d(fprfx, pv)
+    if ( floor(time/timeio) == time/timeio ) then
+      write(*,*) "Model I/O at tstep ", tstep
+      call write_3d('pv        ', pv(1:nx,1:ny,1:nz), (/ nx, ny, nz /), tstep, path_data)
+      call write_3d('u         ', u (1:nx,1:ny,1:nz), (/ nx, ny, nz /), tstep, path_data)
+      call write_3d('v         ', v (1:nx,1:ny,1:nz), (/ nx, ny, nz /), tstep, path_data)
     endif
 
   enddo
@@ -75,35 +142,181 @@ program main
 end program main
 
 subroutine calc_Gpv
-  implicit none
-!INPUT PARAMETERS:  ======================================== 
-  real*8, dimension(nx,ny,nz)   :: dummy
-  character(len=10)             :: dummy 
-!OUTPUT PARAMETERS: ======================================== 
-  real*8, dimension(nx,ny,nz)   :: dummy
-!LOCAL VARIABLES:   ======================================== 
-
-  Gpv = pv*time/t_end
-
-end subroutine calc_Gpv
-
-
-subroutine write_3d(fprfx, outdata)
   use myqg_module
   implicit none
 !INPUT PARAMETERS:  ======================================== 
-  real*8, dimension(nx,ny,nz)   :: outdata
-  character(len=48)             :: fprfx
 !OUTPUT PARAMETERS: ======================================== 
 !LOCAL VARIABLES:   ======================================== 
 
-  open( unit=fid, file=trim(fprfx)//".data", form='unformatted', status='replace', &
-      & access='direct', recl=bytes*nx*ny*nz, convert=endian )
+  !Gpv = -pv/t_end
+  call calc_pvadv
+end subroutine calc_Gpv
+
+subroutine calc_pvadv
+  use myqg_module
+  implicit none
+!INPUT PARAMETERS:  ======================================== 
+!OUTPUT PARAMETERS: ======================================== 
+!LOCAL VARIABLES:   ======================================== 
+  integer :: i,j,k
+  real*8, dimension(1-ox:nx+ox,1-ox:ny+ox,nz) :: uTrans
+  real*8, dimension(1-ox:nx+ox,1-ox:ny+ox,nz) :: vTrans
+  real*8, dimension(1-ox:nx+ox,1-ox:ny+ox,nz) :: fZon
+  real*8, dimension(1-ox:nx+ox,1-ox:ny+ox,nz) :: fMer
+
+  uTrans = 0.0
+  vTrans = 0.0
+  fZon   = 0.0
+  fMer   = 0.0
+
+  ! zonal advective flux
+  do i=1-ox,nx+ox
+    do j=1-ox,ny+ox
+      do k=1,nz
+        uTrans(i,j,k) = 0.25 * ( u(i,j,k) + u(i+1,j,k) + u(i,j+1,k) + u(i+1,j+1,k) )*dy*dz(k)
+      enddo
+    enddo
+  enddo
+
+  do i=1-ox,nx+ox
+    do j=1-ox,ny+ox
+      do k=1,nz
+        fZon(i,j,k) = uTrans(i,j,k) * 0.5 * ( pv(i,j,k)+pv(i+1,j,k) )
+      enddo
+    enddo
+  enddo
+
+  ! meridional advective flux
+  do i=1-ox,nx+ox
+    do j=1-ox,ny+ox
+      do k=1,nz
+        vTrans(i,j,k) = 0.25 * ( v(i,j,k) + v(i+1,j,k) + v(i,j+1,k) + v(i+1,j+1,k) )*dx*dz(k)
+      enddo
+    enddo
+  enddo
+
+  do i=1-ox,nx+ox
+    do j=1-ox,ny+ox
+      do k=1,nz
+        fMer(i,j,k) = vTrans(i,j,k) * 0.5 * ( pv(i,j,k)+pv(i,j+1,k) )
+      enddo
+    enddo
+  enddo
+
+  ! advective tendency
+  do i=1,nx
+    do j=1,ny
+      do k=1,nz
+        Gpv(i,j,k)  = Gpv(i,j,k) -  recepvol(i,j,k) * ( &
+                    & fZon(i+1,j,k) - fZon(i,j,k) + &
+                    & fMer(i,j+1,k) - fMer(i,j,k)   &
+                    & )
+      enddo
+    enddo
+  enddo
+  
+end subroutine calc_pvadv
+
+subroutine cyclic_exchange(var)
+  use myqg_module
+  implicit none
+!INPUT PARAMETERS:  ======================================== 
+!OUTPUT PARAMETERS: ======================================== 
+!LOCAL VARIABLES:   ======================================== 
+  integer :: i,j,k
+  real*8, dimension(1-ox:nx+ox,1-ox:ny+ox,nz), intent(inout) :: var
+
+  if ( cyclic_x .and. cyclic_y ) then
+    do i=1,ox
+      do j=1,ny
+        var(1-i,j,:)   = var(nx+1-i,j,:)
+        var(nx+i,j,:)  = var(i,j,:)
+      enddo
+    enddo
+    do i=1,nx
+      do j=1,ox
+        var(i,1-j,:)   = var(i,ny+1-j,:)
+        var(i,ny+j,:)  = var(i,j,:)
+      enddo
+    enddo
+    do i=1,ox
+      do j=1,ox
+        var(1-i, 1-j, :) = var(nx+1-i,ny+1-j,:)
+        var(1-i, ny+j,:) = var(nx+1-i,j,:)
+        var(nx+i,1-j, :) = var(i,ny+1-j,:)
+        var(nx+i,ny+j,:) = var(i,j,:)
+      enddo
+    enddo
+  else
+    call myqg_error("Both, cyclic_x and cyclic_y, have to be enabled so far")
+  endif
+end subroutine cyclic_exchange
+
+subroutine write_3d(fprfx, outdata, dims, tstepout, path_data)
+  use myqg_module
+  implicit none
+!INPUT PARAMETERS:  ======================================== 
+  character(len=10),            intent(in)  :: fprfx
+  integer, dimension(3),        intent(in)  :: dims
+  real*8, dimension(dims(1),dims(2),dims(3)),  intent(in)  :: outdata
+  integer,                      intent(in)  :: tstepout
+  character(len=128),           intent(in)  :: path_data
+!OUTPUT PARAMETERS: ======================================== 
+!LOCAL VARIABLES:   ======================================== 
+  character(len=128)    :: fname
+  character(len=10)     :: tstepstr 
+
+  !if (.not. present(path_name)) then
+  !  path_name = "./"
+  !endif
+
+! write .data file
+  !if (present(tstepout)) then
+  if (tstepout>0) then
+    write(tstepstr,"(I10.10)") tstepout
+    fname = trim(path_data) // trim(fprfx) // "." // tstepstr // ".data"
+  else
+    fname = trim(path_data) // trim(fprfx) // ".data"
+  endif
+  open( unit=fid, file=fname, form='unformatted', status='replace', &
+      & access='direct', recl=bytes*product(dims), convert=endian )
   write(fid, rec=1) sngl(outdata)
   close(fid)
+  write(*,*) "Write: ", fname
 
-  open( unit=fid, file=trim(fprfx)//".meta", form='formatted', status='replace', &
+! write .meta file
+  !if (present(tstepout)) then
+  if (tstepout>0) then
+    write(tstepstr,"(I10.10)") tstepout
+    fname = trim(path_data) // trim(fprfx) // "." // tstepstr // ".meta"
+  else
+    fname = trim(path_data) // trim(fprfx) // ".meta"
+  endif
+  open( unit=fid, file=fname, form='formatted', status='replace', &
       & access='sequential')
-  write(fid,*) "dims = [", nz, ",", ny, ",", nx, "]"
+  write(fid,*) "nDims = [", 3, "];"
+  write(fid,*) "dimList = [" 
+  write(fid,*) dims(1),",", 1, ",", dims(1), ","
+  write(fid,*) dims(2),",", 1, ",", dims(2), ","
+  write(fid,*) dims(3),",", 1, ",", dims(3)
+  write(fid,*) "];"
+  if ( bytes==4) then
+    write(fid,*) "dataprec = [ 'float32' ];"
+  elseif (bytes==8) then
+    write(fid,*) "dataprec = [ 'float64' ];"
+  endif
+  write(fid,*) "nrecords = [ 1 ];"
+  write(fid,*) "timeStepNumber = [", tstepout, " ];"
+  write(fid,*) "nFlds = [    1 ];"
+  write(fid,*) "fldList = {"
+  write(fid,*) "'", fprfx, "'"
+  write(fid,*) "};"
   close(fid)
 end subroutine write_3d
+
+subroutine myqg_error(errormsg)
+  implicit none
+  character(len=*), intent(in) :: errormsg
+  write(*,*) "::: Error: " // trim(errormsg) // " :::" 
+  stop 1
+end subroutine myqg_error
--- myqg_module.f90
+++ myqg_module.f90
@@ -8,14 +8,24 @@ module myqg_module
   integer :: nx
   integer :: ny
   integer :: nz
+  integer :: ox=1
 
   real*8  :: dx
   real*8  :: dy
-  real*8  :: dz
+  real*8, allocatable, dimension(:)  :: dz
 
   real*8  :: Lx
   real*8  :: Ly
   real*8  :: Lz
+
+  logical :: cyclic_x=.true.
+  logical :: cyclic_y=.true.
+
+  real*8, allocatable, dimension(:)       :: xt, xu
+  real*8, allocatable, dimension(:)       :: yt, yu
+  real*8, allocatable, dimension(:)       :: zt, zu
+
+  real*8, allocatable, dimension(:,:,:)   :: recepvol 
 
 ! time stepping parameters
   real*8  :: dt
@@ -23,12 +33,8 @@ module myqg_module
   real*8  :: t_start
   real*8  :: t_end
   real*8  :: time
-
-  real*8, allocatable, dimension(:)       :: xt, xu
-  real*8, allocatable, dimension(:)       :: yt, yu
-  real*8, allocatable, dimension(:)       :: zt, zu
-
-  logical :: testlog
+  real*8  :: timeio
+  real*8  :: epsab=0.01
 
 ! mpi parameters
   integer :: numpr
@@ -43,10 +49,11 @@ module myqg_module
   real*8, allocatable, dimension(:,:,:)   :: pt
 
 ! tendencies for diagnostic variables
-  real*8, allocatable, dimension(:,:,:)   :: Gpv  
+  real*8, allocatable, dimension(:,:,:)   :: Gpv
+  real*8, allocatable, dimension(:,:,:)   :: Gpvm1
   real*8, allocatable, dimension(:,:,:)   :: Gpt
+  real*8, allocatable, dimension(:,:,:)   :: Gptm1
  
-
 ! parameters for I/O
   character(len=24) :: endian = "big_endian"
   integer           :: bytes  = 4
@@ -64,13 +71,17 @@ subroutine allocate_myqg_module
   allocate( xt(nx), xu(nx) ); xt=0; xu=0
   allocate( yt(ny), yu(ny) ); yt=0; yu=0
   allocate( zt(nz), zu(nz) ); zt=0; zu=0
+  allocate( dz(nz) ); dz=0
 
-  allocate( u(nx,ny,nz), v(ny,ny,nz) ); u=0; v=0
-  allocate( w(nx,ny,nz), p(ny,ny,nz) ); w=0; p=0
-  allocate( b(nx,ny,nz) ); b=0
-  allocate( psi(nx,ny,nz), pv(ny,ny,nz) ); psi=0; pv=0
-  allocate( pt(nx,ny,nz) ); pt=0
-  allocate( Gpv(nx,ny,nz) ); Gpv=0
-  allocate( Gpt(nx,ny,nz) ); Gpt=0
+  allocate( recepvol(1:nx,1:ny,1:nz) ); recepvol=0
+
+  allocate( u(1-ox:nx+ox,1-ox:ny+ox,nz), v(1-ox:nx+ox,1-ox:ny+ox,nz) ); u=0; v=0
+  allocate( w(1-ox:nx+ox,1-ox:ny+ox,nz), p(1-ox:nx+ox,1-ox:ny+ox,nz) ); w=0; p=0
+  allocate( b(1-ox:nx+ox,1-ox:ny+ox,nz) ); b=0
+  allocate( psi(1-ox:nx+ox,1-ox:ny+ox,nz), pv(1-ox:nx+ox,1-ox:ny+ox,nz) ); psi=0; pv=0
+  allocate( pt(1-ox:nx+ox,1-ox:ny+ox,nz) ); pt=0
+  !allocate( Gpv(1-ox:nx+ox,1-ox:ny+ox,nz), Gpvm1(1-ox:nx+ox,1-ox:ny+ox,nz) ); Gpv=0; Gpvm1=0
+  allocate( Gpv(nx,ny,nz), Gpvm1(nx,ny,nz) ); Gpv=0; Gpvm1=0
+  allocate( Gpt(1-ox:nx+ox,1-ox:ny+ox,nz), Gptm1(1-ox:nx+ox,1-ox:ny+ox,nz) ); Gpt=0; Gptm1=0
 
 end subroutine allocate_myqg_module
